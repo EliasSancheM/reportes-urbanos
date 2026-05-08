13e04e34b262efabf8b98616adbf16f8e23fe18b
Estudiante <estudiante@universidad.edu>
2026-05-07 22:52:54 -0400

    Fix Express wildcard route for path-to-regexp v8

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use('/api/reports', reportRoutes);
 // Servir el frontend estático en producción
 app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
+app.get(/(.*)/, (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
